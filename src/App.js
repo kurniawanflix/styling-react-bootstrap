71a839f4d64a26d3a81523039b4a093511376da2
kurniawanflix <kurniawanflix@gmail.com>
2022-09-13 21:31:29 +0700

    upload project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navigationbar from "./components/Navbar";
+import Header from "./components/Header";
+import Jumbotron from "./components/Jumbroton";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigationbar />
+      <Header />
+      <div className="container">
+        <Jumbotron />
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
